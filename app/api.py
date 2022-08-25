d1750dbf9c2fad4c85e3004ddcdb9723428da3bb
kzrkhan <khizer.khan98@gmail.com>
2022-08-26 00:47:54 +0500

    CORS Policy updated. Image backend tested

--- app/api.py
+++ app/api.py
@@ -2,14 +2,18 @@
 # In memory of Do Deeee
 
 from datetime import date
+import time
 import os
-from fastapi import FastAPI, Depends, Query, Body
+import shutil
+from fastapi import FastAPI, Depends, Query, Body, UploadFile, File
 from pydantic import BaseModel
 from typing import Optional
 from app.models import PostSchema, UserLoginSchema, UserSchema
 from app.auth.auth_handler import sign_JWT
 from app.auth.auth_bearer import JWTBearer
 from supabase import create_client, Client
+from pyuploadcare import Uploadcare
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI(
@@ -19,10 +23,20 @@ app = FastAPI(
 )
 
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 url: str = os.environ.get("DB_URL")
 key: str = os.environ.get("DB_KEY")
 
 supabase: Client = create_client(url, key)
+
+
+uploadcare = Uploadcare(public_key="f1f4d42015a1df04ce31", secret_key="a9492dac9132302f4360")
 
 
 posts = [
@@ -91,3 +105,24 @@ def check_user(data : UserLoginSchema):
     
     except:
         return False
+
+
+@app.post('/file')
+async def upload_file(file: UploadFile = File(...)):
+    
+    with open(f'{file.filename}', 'wb') as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    file_name = file.filename
+    print(file_name)
+
+    f_up = open(file_name, 'rb')
+    ucare_file: File = uploadcare.upload(f_up)
+    f_up.close()
+
+    try:
+        os.remove(file_name)
+    except:
+        pass
+
+    return {"file_url" : str(ucare_file)}
